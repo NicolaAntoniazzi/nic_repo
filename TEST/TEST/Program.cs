c3ded6a54983d0e8633bee0379990c538e877708
Nicola Antoniazzi <nicola.antoniazzi@gmail.com>
2015-04-28 10:17:59 +0200

    test di add

---
+++ TEST/TEST/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                switch (i)
+                {
+                    case 1: Console.WriteLine(" ****  ***  ***    ***  ***  *    * ***  **");
+                        break;
+                    case 2 : Console.WriteLine(" *   *  *  *   *  *    *   * **   * *    ** ");
+                        break;
+                    case 3: Console.WriteLine(" *   *  *  *   *  *    ***** * *  * **   **");
+                        break;
+                    case 4: Console.WriteLine(" *   *  *  *   *  *    *   * *   ** *    ");
+                        break;
+                    case 5: Console.WriteLine(" ****  ***  ***    *** *   * *    * ***  **");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
